bbb9e6b616231e63e3e7d4b9b08d83221a763758
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-11-08 14:39:44 -0600

    work around minix 3.3.0 stable and 3.4.0rc6 bugs

--- minix/cleanup.minix.sh
+++ minix/cleanup.minix.sh
@@ -3,16 +3,3 @@
 # Clear temporary files
 
 rm -rf /tmp/*
-
-# # Shrink home partition
-#
-# dd if=/dev/zero of=/home/whitespace bs=1M ||
-#     echo 'Zeroed home partition' &&
-#     rm -f /home/whitespace
-#
-# # Shrink root partition and persist disks
-#
-# dd if=/dev/zero of=/whitespace bs=1M ||
-#     echo 'Zeroed disk' &&
-#     rm -f /whitespace &&
-#     sync
