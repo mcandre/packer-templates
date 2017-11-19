2b7d89496b5a396478d8c8153abcf5e34c0dc0d9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-11-19 10:03:35 -0600

    speed up packing by moving vagrant ssh authentication to post-preseed script

---
+++ freebsd/authenticate-vagrant.freebsd.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pkg update &&
+    pkg install -y wget ca_root_nss &&
+    mkdir -p /home/vagrant/.ssh &&
+    wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
+    chown -R vagrant:vagrant /home/vagrant/.ssh &&
+    chmod 0700 /home/vagrant/.ssh &&
+    chmod 0600 /home/vagrant/.ssh/authorized_keys &&
+    pkg delete -y wget ca_root_nss
