8f81d5ef5f59817982d6b8ef4f38ced7497f2640
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-12-28 21:24:05 -0600

    debug debian synced folders

---
+++ debian/virtualbox-install-drivers.debian.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+case "$PACKER_BUILDER_TYPE" in
+    virtualbox-*)
+        ;;
+    *)
+        exit
+esac
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update &&
+    apt-get install -y "linux-headers-$(uname -r)" build-essential &&
+    mkdir /tmp/VirtualBox &&
+    mount -o loop "/home/vagrant/VBoxGuestAdditions.iso" /tmp/VirtualBox &&
+    yes | sh /tmp/VirtualBox/VBoxLinuxAdditions.run &&
+    umount /tmp/VirtualBox &&
+    rmdir /tmp/VirtualBox &&
+    service vboxadd start &&
+    rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.? &&
+    rm -rf /usr/src/virtualbox-ose-guest* &&
+    rm -rf /usr/src/vboxguest* &&
+    apt-get purge -y "linux-headers-$(uname -r)" build-essential &&
+    rm /home/.vagrant/.vbox_version
