71435f1946355b732c8658e17965b09e018be76c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-12-31 17:44:45 -0600

    debug sudo

--- debian/fix-sudo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-groupadd wheel &&
-    usermod -G wheel root &&
-    usermod -G wheel vagrant &&
-    printf "%%wheel ALL=(ALL:ALL) NOPASSWD:ALL\nDefaults !requiretty\n" >>/etc/sudoers &&
-    chmod 0440 /etc/sudoers
