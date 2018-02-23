eee17e78f7f55095f8c13c991cd3467447bc025f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-02-22 19:25:29 -0600

    prefer rsync for shared folders

--- windows/fix-virtual-drivers.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-If ( $Env:PACKER_BUILD_TYPE -eq "virtualbox-iso" ) {
-  certutil -addstore -f TrustedPublisher E:\cert\oracle-vbox.cer
-  E:\VBoxWindowsAdditions.exe
-  eject E:
-}
