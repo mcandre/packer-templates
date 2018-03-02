9c253c0164f7b5ca9af6d673a2ff7a23cd3d48bb
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-03-02 12:11:09 -0600

    close #144

--- smartos/fix-vagrant-rsync.smartos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir /opt/vagrant &&
-    chown vagrant:vagrant /opt/vagrant
--- smartos/ssh-configure-vagrant-key.smartos.sh
+++ smartos/ssh-configure-vagrant-key.smartos.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-mkdir -p /opt/home/vagrant/.ssh &&
-    curl -kLo /opt/home/vagrant/.ssh/authorized_keys 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' &&
-    digest -a md5 /opt/home/vagrant/.ssh/authorized_keys | grep 'b440b5086dd12c3fd8abb762476b9f40' &&
+mkdir -p /usbkey/config.inc &&
+    curl -kLo /usbkey/config.inc/authorized_keys 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' &&
+    digest -a md5 /usbkey/config.inc/authorized_keys | grep 'b440b5086dd12c3fd8abb762476b9f40' &&
+    echo 'root_authorized_keys_file=authorized_keys' >>/usbkey/config &&
+    mkdir /opt/home/vagrant/.ssh &&
+    cp /usbkey/config.inc/authorized_keys /opt/home/vagrant/.ssh/authorized_keys &&
     chown -R vagrant:staff /opt/home/vagrant/.ssh &&
     chmod 0755 /opt/home/vagrant/.ssh &&
-    chmod 0600 /opt/home/vagrant/.ssh/authorized_keys &&
-    mkdir -p /usbkey/config.inc
+    chmod 0600 /opt/home/vagrant/.ssh/authorized_keys
