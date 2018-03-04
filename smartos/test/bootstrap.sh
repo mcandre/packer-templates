67c0353f09925e2214c8c5cb77e7578e54e70150
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-03-04 10:16:02 -0600

    fix persistence

--- smartos/test/bootstrap.sh
+++ smartos/test/bootstrap.sh
@@ -5,4 +5,5 @@ pfexec \
     'https://raw.githubusercontent.com/mcandre/doobie/master/lib/doobie.sh' &&
     digest -a md5 /opt/doobie.sh |
     grep 'f446e12c0a603bbde9fd51324edbf092' >/dev/null &&
-    chmod 644 /opt/doobie.sh
+    chmod 644 /opt/doobie.sh &&
+    echo '. /opt/doobie.sh' >>/opt/home/vagrant/.profile
