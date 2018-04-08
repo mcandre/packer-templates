4aaedc83af87623233234d12a9deaa12fc584df6
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-04-08 09:47:35 -0500

    debug packer stuck at starting remote command: test -d

--- openwrt/fix-vagrant-ssh.openwrt.sh
+++ openwrt/fix-vagrant-ssh.openwrt.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 opkg update &&
-    opkg install libustream-openssl ca-certificates &&
+    opkg install wget libustream-openssl ca-certificates &&
     mkdir -p /home/vagrant/.ssh &&
     wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
     chown -R vagrant:vagrant /home/vagrant/.ssh &&
     chmod 0700 /home/vagrant/.ssh &&
     chmod 0600 /home/vagrant/.ssh/authorized_keys &&
-    opkg remove --autoremove libustream-openssl ca-certificates
+    opkg remove --autoremove wget libustream-openssl ca-certificates
