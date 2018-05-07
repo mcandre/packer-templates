3b6f15527cb0aa58356fdeff7f86a9ca4835138b
Zach Crownover <zachary.crownover@gmail.com>
2018-05-07 02:39:18 +0000

    Remove unneded wget
    DragonFly BSD and FreeBSD both come with fetch(1), a utility which is
    very similar to wget, so installing wget to pull one file to then
    delete it again is entirely unnecessary. This saves a fetch, and a
    package deletion, and changes the command invocation to use the
    existing fetch command.

--- dragonflybsd/ssh-configure-vagrant-key.dragonflybsd.sh
+++ dragonflybsd/ssh-configure-vagrant-key.dragonflybsd.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 pkg update &&
-    pkg install -y wget &&
     mkdir -p /home/vagrant/.ssh &&
-    wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
+    fetch -o /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
     chown -R vagrant:vagrant /home/vagrant/.ssh &&
     chmod 0700 /home/vagrant/.ssh &&
     chmod 0600 /home/vagrant/.ssh/authorized_keys &&
-    pkg delete -y wget
