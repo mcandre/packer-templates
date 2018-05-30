183d5549dd1354907a06f34b13b9817970d7f2a9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-05-30 13:31:56 -0500

    debug dns client operation, debug boot prompts, debug netssh opening port 22, debug packer ssh

--- plan9/fix-vagrant-ssh.plan9.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p /home/vagrant/.ssh &&
-    wget -O /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
-    chown -R vagrant:vagrant /home/vagrant/.ssh &&
-    chmod 0700 /home/vagrant/.ssh &&
-    chmod 0600 /home/vagrant/.ssh/authorized_keys
