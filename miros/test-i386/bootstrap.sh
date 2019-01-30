f37f3b957a815e43432508cd96e1895d1b81fa34
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-01-29 21:02:24 -0600

    fix test

--- miros/test-i386/bootstrap.sh
+++ miros/test-i386/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo env PKG_PATH=https://www.mirbsd.org/MirOS/v10/Pkgs/i386 /usr/mpkg/sbin/pkg_add wget-1.10.2-2.cgz
+sudo env PKG_PATH=https://www.mirbsd.org/MirOS/v10/Pkgs/i386 /usr/mpkg/sbin/pkg_add wget-1.10.2-2
