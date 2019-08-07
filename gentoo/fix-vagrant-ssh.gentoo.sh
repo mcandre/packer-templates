50b61b80aa6389eed2bc40edba922c0bc441fe0d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2019-08-06 19:53:14 -0500

    fix notty

--- gentoo/fix-vagrant-ssh.gentoo.sh
+++ gentoo/fix-vagrant-ssh.gentoo.sh
@@ -6,5 +6,5 @@ emerge -uDU --keep-going --with-bdeps=y @world &&
     chown -R vagrant:vagrant /home/vagrant/.ssh &&
     chmod 0700 /home/vagrant/.ssh &&
     chmod 0600 /home/vagrant/.ssh/authorized_keys &&
-    emerge -cav wget ca-certificates &&
+    emerge wget ca-certificates &&
     emerge --depclean
