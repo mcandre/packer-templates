ac7be38a8887b42f586837bf4388b6b010c3661d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-02-26 19:31:49 -0600

    fix dfly

--- dragonflybsd/ssh-configure-vagrant-key.dragonflybsd.sh
+++ dragonflybsd/ssh-configure-vagrant-key.dragonflybsd.sh
@@ -4,4 +4,4 @@ pkg update &&
     fetch -o /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub &&
     chown -R vagrant:vagrant /home/vagrant/.ssh &&
     chmod 0700 /home/vagrant/.ssh &&
-    chmod 0600 /home/vagrant/.ssh/authorized_keys &&
+    chmod 0600 /home/vagrant/.ssh/authorized_keys
